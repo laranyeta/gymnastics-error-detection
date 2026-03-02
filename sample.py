cbcf2255eb22dbcb3893298f53f4404bc5299ba9
Lara Castillejo Roig <92856231+laranyeta@users.noreply.github.com>
2026-03-02 15:37:39 +0100

    add: main files

---
+++ sample.py
@@ -0,0 +1,7 @@
+#### TESTING SAMPLE (WIP)
+## first samples and test will be shown through this code
+## via balance beam videos
+from utils.data import read_video
+from utils.visualization import *
+
+read_video("dataset/video_test/front_aerial.mp4")

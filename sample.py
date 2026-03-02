6c496b28ff40acc4fa50f0280ffca09d6f6b3567
Lara Castillejo Roig <92856231+laranyeta@users.noreply.github.com>
2026-03-02 15:40:52 +0100

    update: added filtering logic

--- sample.py
+++ sample.py
@@ -1,7 +1,8 @@
 #### TESTING SAMPLE (WIP)
 ## first samples and test will be shown through this code
 ## via balance beam videos
-from utils.data import read_video
+from utils.data import read_video, filter_annotations
 from utils.visualization import *
 
+filter_annotations("dataset/original/finegym_v1.1.json")
 read_video("dataset/video_test/front_aerial.mp4")

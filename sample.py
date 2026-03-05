a391462f7fb5bbb283a8b3993ca00c4ed8d96a17
Lara Castillejo Roig <92856231+laranyeta@users.noreply.github.com>
2026-03-05 10:50:14 +0100

    update: added automatic filename changing

--- sample.py
+++ sample.py
@@ -4,9 +4,11 @@ import os
 from utils.data import read_video, save_video, filter_annotations
 from utils.visualization import extract_pose, draw_skeleton
 
+input_path = "dataset/video_test/tuck_backflip.mp4"
+filename = input_path[19:-4]
 output_path = "outputs"
 filter_annotations("dataset/original/finegym_v1.1.json")
-frames = read_video("dataset/video_test/flicflac_backflip.mp4") #every frame of the video
+frames = read_video(input_path) #every frame of the video
 print(f"[DEBUG] {len(frames)} frames have been read.")
 
 result = []
@@ -16,4 +18,4 @@ for i, frame in enumerate(frames):
     result.append(pose_estimation)
 
 os.makedirs(output_path, exist_ok=True)
-save_video(result, output_path)
+save_video(result, f"{output_path}/{filename}_out.avi")

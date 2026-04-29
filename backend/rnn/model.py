35db93d85679ac35d2e5de4678212ad6d044d28b
Lara Castillejo Roig <lara@laras-macbook-air.local>
2026-04-29 19:23:45 +0200

    update: evaluation per epoch

--- backend/rnn/model.py
+++ backend/rnn/model.py
@@ -60,29 +60,39 @@ class GymnasticsDataset(Dataset):
 def train():
     if torch.backends.mps.is_available():
         device = 'mps' #macos compatibility
-    else:
+    elif torch.cuda.is_available():
         device = 'cuda' #windows compatibility
+    else:
+        device = 'cpu'
+        
+    print(f"[SYSTEM] Running on device: {device}")
     
-    input_size = 206 #33 keypoints*2 (x,y) * 3 (x,v,a) + 8 ang
+    input_size = 206 # 33 keypoints*2 (x,y) * 3 (x,v,a) + 8 ang
     hidden_size = 128
-    n_classes = len(LABEL_MAPPING) #4
+    n_classes = len(LABEL_MAPPING) # 4
     n_layers = 2
     epochs = 50
     batch_size = 1 
 
     dataset = GymnasticsDataset("data")
-    loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    train_size = int(0.8 * len(dataset)) #80% train
+    val_size = len(dataset) - train_size #20% validation
+
+    torch.manual_seed(42) #same split for each execution
+    train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
     
     model = RNNAcrobaticClassificator(input_size, hidden_size, n_classes, n_layers).to(device)
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=0.001)
 
     for epoch in range(epochs):
-        model.train()
+        model.train() #training phase
         total_loss = 0
-        for X_batch, y_batch in loader:
-            X_batch = X_batch.to(device)
-            y_batch = y_batch.to(device)
+        for X_batch, y_batch in train_loader:
+            X_batch, y_batch = X_batch.to(device), y_batch.to(device)
             
             optimizer.zero_grad()
             outputs = model(X_batch)
@@ -91,8 +101,28 @@ def train():
             optimizer.step()
             total_loss += loss.item()
 
+        model.eval() #evaluation phase
+        val_loss = 0
+        correct = 0
+        total = 0
+        
+        with torch.no_grad():
+            for X_val, y_val in val_loader:
+                X_val, y_val = X_val.to(device), y_val.to(device)
+                
+                outputs = model(X_val)
+                loss = criterion(outputs, y_val)
+                val_loss += loss.item()
+                
+                _, y_pred = torch.max(outputs.data, 1)
+                total += y_val.size(0)
+                correct += (y_pred == y_val).sum().item()
+
         if (epoch+1) % 5 == 0: 
-            print(f"Epoch [{epoch+1}/{epochs}], Loss:  {total_loss/len(loader):.4f}")
+            train_loss_avg = total_loss / len(train_loader)
+            val_loss_avg = val_loss / len(val_loader)
+            accuracy = 100 * correct / total
+            print(f"Epoch [{epoch+1}/{epochs}] | Train Loss: {train_loss_avg:.4f} | Val Loss: {val_loss_avg:.4f} | Val Acc: {accuracy:.2f}%")
 
     os.makedirs("checkpoints", exist_ok=True)
     torch.save(model.state_dict(), "checkpoints/best.pth")

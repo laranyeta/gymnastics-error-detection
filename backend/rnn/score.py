e9e200a6b71f33957503f7a94a87efb50823c63f
Lara Castillejo Roig <lara@laras-macbook-air.local>
2026-05-12 13:27:29 +0200

    update: readded peak acrobatic for evaluation

--- backend/rnn/score.py
+++ backend/rnn/score.py
@@ -5,17 +5,6 @@ import math
 from backend.scoring.evaluator import AcrobaticEvaluator
 from backend.rnn.predict import predict
 
-'''def find_acrobatic_peak(sequence, pred):
-    if pred in ["split", "straddle"]:
-        idx, peak_frame = max(enumerate(sequence), key=lambda x: x[1]["angles"].get("opening_L", 0))
-    elif pred in ["tuck", "pike"]:
-        idx, peak_frame = min(enumerate(sequence), key=lambda x: x[1]["angles"].get("joint_hip_L", 180)) 
-    else:
-        idx = len(sequence) // 2
-        peak_frame = sequence[idx] 
-        
-    return idx, peak_frame'''
-
 def calculate_split_angle(pos): #calculates the real angle by making an imaginary keypoint for extra vectorial angle calculation
     hip_L_x, hip_L_y = pos.get("x_23", 0), pos.get("y_23", 0)
     hip_R_x, hip_R_y = pos.get("x_24", 0), pos.get("y_24", 0)
@@ -40,14 +29,25 @@ def calculate_split_angle(pos): #calculates the real angle by making an imaginar
 
     return (math.degrees(math.acos(cos_theta))) #radians -> degrees
 
-def evaluate_performance(json_path, pred, frame_idx):
+def find_acrobatic_peak(sequence, pred):
+    if pred in ["split", "straddle"]:
+        idx, peak_frame = max(enumerate(sequence), key=lambda x: calculate_split_angle(x[1].get("position", {})))
+    elif pred in ["tuck", "pike"]:
+        idx, peak_frame = min(enumerate(sequence), key=lambda x: x[1]["angles"].get("joint_hip_L", 180)) 
+    else:
+        idx = len(sequence) // 2
+        peak_frame = sequence[idx] 
+        
+    return idx, peak_frame
+
+def evaluate_performance(json_path, pred):
     with open(json_path, 'r') as f:
         sequence = json.load(f)
         
     evaluator = AcrobaticEvaluator()
-    #peak_idx, peak_frame = find_acrobatic_peak(sequence, pred)
-    angles = sequence[frame_idx].get("angles", {})
-    pos = sequence[frame_idx].get("position", {})
+    peak_idx, _ = find_acrobatic_peak(sequence, pred)
+    angles = sequence[peak_idx].get("angles", {})
+    pos = sequence[peak_idx].get("position", {})
     
     penalty = 0.0
     if pred == "tuck":
@@ -86,15 +86,15 @@ def evaluate_performance(json_path, pred, frame_idx):
         print(f"Knee R angle: {knee_R:.2f}")
         penalty, breakdown = evaluator.eval_straddle(opening_angle, knee_L, knee_R)
 
-    return penalty, breakdown
+    return penalty, breakdown, peak_idx
 
-def visualize_peak_frame(video_path, frame_idx, output_img="peak_frame.jpg"):
+def visualize_peak_frame(video_path, peak_frame):
     cap = cv2.VideoCapture(video_path)
-    cap.set(cv2.CAP_PROP_POS_FRAMES, frame_idx)
+    cap.set(cv2.CAP_PROP_POS_FRAMES, peak_frame)
     
     ret, frame = cap.read()
     if ret:
-        cv2.imshow(f"Peak Frame - Index: {frame_idx}", frame)
+        cv2.imshow(f"Peak Frame - Index: {peak_frame}", frame)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
     else:
@@ -106,16 +106,15 @@ def visualize_peak_frame(video_path, frame_idx, output_img="peak_frame.jpg"):
 if __name__ == "__main__":
     json_path = "backend/rnn/test/test02.json"
     d_score = 5.0 #hardcoded, d-score is not automatized
-    frame = 40
 
     pred = predict(json_path, debug=False)
     
     print("\n--- GYMNASTICS EVALUATION REPORT ---")
-    penalty, breakdown = evaluate_performance(json_path, pred, frame)
+    penalty, breakdown, peak_idx = evaluate_performance(json_path, pred)
     
     evaluator = AcrobaticEvaluator()
     final_score = evaluator.calculate_final_score(d_score, penalty)
-    visualize_peak_frame("backend/rnn/test/test02_skeleton.avi", frame)
+    visualize_peak_frame("backend/rnn/test/test02_skeleton.avi", peak_idx)
     print(f"Detected Acrobatic: {pred.upper()}")
     
     print(f"\n--- DEDUCTION BREAKDOWN ---")
